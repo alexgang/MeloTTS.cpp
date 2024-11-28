6695cb329d68a6b97504452c98027872825aa837
qiutong <tong.qiu@intel.com>
2024-11-28 12:28:59 +0000

    add openvino_tokenizer for en and zh and testing

--- tests/test_openvino_tokenizer.cpp
+++ tests/test_openvino_tokenizer.cpp
@@ -11,6 +11,8 @@
 #include <windows.h>
 #endif
 #include <openvino/openvino.hpp>
+#include "../experimental/openvino_tokenizer.h"
+
 //#include <openvino/op/transpose.hpp>
 //#include <openvino/core/node.hpp>
 #include "utils.h"
@@ -20,84 +22,28 @@ class TokenizerTestSuit : public ::testing::Test {
 public:
     TokenizerTestSuit() {
         std::filesystem::path model_path = "C:\\Users\\gta\\source\\repos\\MeloTTS.cpp\\ov_models";
-        std::filesystem::path bert_subword_tokenizer = model_path / ".." / "experimental" / "bert-base-multilingual-uncased" / "bert_subword" / "bert_subword_tokenizer.xml";
-        std::filesystem::path bert_subword_detokenizer = model_path / ".." / "experimental" / "bert-base-multilingual-uncased" / "bert_subword" / "bert_subword_detokenizer.xml";
+        std::filesystem::path zh_bert_subword_tokenizer = model_path / ".." / "experimental" / "bert-base-multilingual-uncased" / "bert_subword" / "bert_subword_tokenizer.xml";
+        std::filesystem::path zh_bert_subword_detokenizer = model_path / ".." / "experimental" / "bert-base-multilingual-uncased" / "bert_subword" / "bert_subword_detokenizer.xml";
+        std::filesystem::path en_bert_subword_tokenizer = model_path / ".." / "experimental" / "bert-base-uncased" /  "bert_subword_tokenizer.xml";
+        std::filesystem::path en_bert_subword_detokenizer = model_path / ".." / "experimental" / "bert-base-uncased" /  "bert_subword_detokenizer.xml";
         std::filesystem::path dll_path = "C:\\Users\\gta\\source\\repos\\openvino_tokenizers_windows_2024.5.0.0_x86_64\\runtime\\bin\\intel64\\Release\\openvino_tokenizers.dll";
-        core.add_extension(dll_path.string().c_str());
-      
-        subword_tokenizer_infer = core.compile_model(bert_subword_tokenizer.string()).create_infer_request();
-        subword_detokenizer_infer = core.compile_model(bert_subword_detokenizer.string()).create_infer_request();
-    }
-    ov::Tensor tokenize(std::string&& prompt) {
-        subword_tokenizer_infer.set_input_tensor(ov::Tensor{ ov::element::string, {1}, &prompt });
-        subword_tokenizer_infer.infer();
-        return  subword_tokenizer_infer.get_tensor("input_ids");
-    }
+        core = std::make_unique<ov::Core>();
+        zh_tokenizer = OpenVinoTokenizer(core,dll_path,zh_bert_subword_tokenizer,zh_bert_subword_detokenizer);
+        en_tokenizer = OpenVinoTokenizer(core,dll_path,en_bert_subword_tokenizer,en_bert_subword_detokenizer);
 
-    std::string* detokenize(int64_t&& token_id) {
-        constexpr size_t BATCH_SIZE = 1;
-        subword_detokenizer_infer.set_input_tensor(ov::Tensor{ ov::element::i64, {BATCH_SIZE,1}, &token_id });
-        subword_detokenizer_infer.infer();
-        return subword_detokenizer_infer.get_output_tensor().data<std::string>();
     }
-    std::vector<std::string> detokenize(std::vector<int64_t>&& token_id, size_t size) {
-        constexpr size_t BATCH_SIZE = 1;
-        subword_detokenizer_infer.set_input_tensor(ov::Tensor{ ov::element::i64, {BATCH_SIZE,size}, token_id.data() });
-        subword_detokenizer_infer.infer();
-        size_t len = subword_detokenizer_infer.get_output_tensor().get_shape()[0];
-        std::vector<std::string> res;
-        std::string* ptr = subword_detokenizer_infer.get_output_tensor().data<std::string>();
-        for (int i = 0; i < len; ++i)
-            res.emplace_back(*(ptr + i));//subword_detokenizer_infer.get_output_tensor().data<std::string>()[i]
-        return res;
-    }
-    std::vector<std::string> detokenize(ov::Tensor& token_ids) {
-        subword_detokenizer_infer.set_input_tensor(token_ids);
-        subword_detokenizer_infer.infer();
-        size_t len = subword_detokenizer_infer.get_output_tensor().get_shape()[0];
-        std::vector<std::string> res;
-        std::string* ptr = subword_detokenizer_infer.get_output_tensor().data<std::string>();
-        for (int i = 0; i < len; ++i) {
-            // ignore the [CLS] token at the beginning and the [SEP] token at the end.
-            if (*(ptr + i) == "[CLS]" || *(ptr + i) == "[SEP]")
-                continue;
-            res.emplace_back(*(ptr + i));
-        }
-        return res;
-    }
+  
 
-    /**
-     * @brief Tokenizes the given prompt into subwords.
-     *
-     * This function takes a string prompt and tokenizes it into subwords using the OpenVINO tokenizer.
-     *
-     * @param prompt The input string to be tokenized.
-     * @return A vector of strings, where each string is a subword token.
-     */
-    std::vector<std::string> subword_tokenize(std::string& prompt) {
-        ov::Tensor token_ids = tokenize(std::move(prompt));
-        return detokenize(token_ids);
-    }
-    template<typename T>
-    std::vector<T> get_output_vec(const ov::Tensor& output_tensor) {
-        const T* output_data = output_tensor.data<T>();
-        size_t frame_num = output_tensor.get_shape()[1];
-        std::cout << output_tensor.get_shape() << std::endl;
-        std::vector<T> res(frame_num);
-        for (size_t i = 0; i < frame_num; ++i) {
-            res[i] = output_data[i];
-        }
-        return res;
-    }
 protected:
-    ov::Core core;
-    ov::InferRequest subword_tokenizer_infer, subword_detokenizer_infer;
+    std::unique_ptr<ov::Core> core;
+    OpenVinoTokenizer zh_tokenizer, en_tokenizer;
+    //ov::InferRequest zh_subword_tokenizer_infer, zh_subword_detokenizer_infer, en_subword_tokenizer_infer, en_subword_detokenizer_infer;
 
 
 };
 
 
-TEST_F(TokenizerTestSuit, BertSubwordTokenizer) {
+TEST_F(TokenizerTestSuit, ZH_BertSubwordTokenizer) {
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
 #endif
@@ -106,17 +52,17 @@ TEST_F(TokenizerTestSuit, BertSubwordTokenizer) {
     std::vector<std::string> tokens;
     std::vector<int64_t> token_ids;
     auto startTime = Time::now();
-    ov::Tensor res = tokenize(std::move(text));
-    auto vec = get_output_vec<int64_t>(res);
-    auto execTime = get_duration_us_till_now(startTime);
-    std::cout << "[INFO] subword_tokenize takes "<< execTime<<"us\n";
+    ov::Tensor res = zh_tokenizer.tokenize_tensor(std::move(text));
+    auto execTime = get_duration_ms_till_now(startTime);
+    auto vec = OpenVinoTokenizer::get_output_vec<int64_t>(res);
+    std::cout << "[INFO] subword_tokenize takes "<< execTime<<"ms\n";
     const std::vector<int64_t> correct_ids = { 101, 6784, 7984, 2693, 85065, 33719, 1817, 3295, 2415, 6990, 1776, 2160, 4270, 3203, 2383, 18958, 59242, 4108, 3259, 6805,
         2981, 5975, 4767, 4508, 3203, 2383, 79947, 20849, 59242, 102 };
 
     EXPECT_EQ(vec, correct_ids);
 }
 
-TEST_F(TokenizerTestSuit, BertSubwordDeTokenizer) {
+TEST_F(TokenizerTestSuit, ZH_BertSubwordDeTokenizer) {
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
 #endif
@@ -133,17 +79,17 @@ TEST_F(TokenizerTestSuit, BertSubwordDeTokenizer) {
     std::vector<std::string> res, res1;
 
     for (auto& id : input_ids) {
-        res.emplace_back(*detokenize(std::move(id)));
+        res.emplace_back(*zh_tokenizer.detokenize(std::move(id)));
     }
-
-    res1 = detokenize(std::move(input_ids), input_ids.size());
-
-    
+    auto startTime = Time::now();
+    res1 = zh_tokenizer.detokenize(std::move(input_ids), input_ids.size());
+    auto execTime = get_duration_ms_till_now(startTime);
+    std::cout << "[INFO] detokenize takes " << execTime << "ms\n";
     EXPECT_EQ(res, correct_subwords);
     EXPECT_EQ(res1, correct_subwords);
 }
 //https://github.com/huggingface/transformers/blob/main/docs/source/en/tokenizer_summary.md#subword-tokenization
-TEST_F(TokenizerTestSuit, Subword_tokenization) {
+TEST_F(TokenizerTestSuit, ZH_Subword_tokenization) {
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
 #endif
@@ -155,9 +101,47 @@ TEST_F(TokenizerTestSuit, Subword_tokenization) {
     "推", "导", "缺", "失", "的", "模", "板", "实", "参",
     "temp", "##late", "arguments" };
     auto startTime = Time::now();
-    std::vector<std::string> res = subword_tokenize(text);
-    auto execTime = get_duration_us_till_now(startTime);
-    std::cout << "[INFO] split subword takes " << execTime << "us\n";
+    std::vector<std::string> res = zh_tokenizer.word_segment(text);
+    auto execTime = get_duration_ms_till_now(startTime);
+    std::cout << "[INFO] split subword takes " << execTime << "ms\n";
+
+    EXPECT_EQ(res, correct_subwords);
+}
+
+//https://github.com/huggingface/transformers/blob/main/docs/source/en/tokenizer_summary.md#subword-tokenization
+TEST_F(TokenizerTestSuit, EN_BertSubwordDeTokenizer) {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+    std::string text = "I've been learning machine learning recently and hope to make contributions in the field of artificial intelligence in the future.";
+    std::cout << text << std::endl;
+    std::vector<std::string> tokens;
+    std::vector<int64_t> token_ids;
+    auto startTime = Time::now();
+    ov::Tensor res = en_tokenizer.tokenize_tensor(std::move(text));
+    auto execTime = get_duration_ms_till_now(startTime);
+    auto vec = OpenVinoTokenizer::get_output_vec<int64_t>(res);
+    std::cout << "[INFO] subword_tokenize takes " << execTime << "ms\n";
+    const std::vector<int64_t> correct_ids = { 101, 1045, 1005, 2310, 2042, 4083, 3698, 4083, 3728, 1998, 3246, 2000,
+         2191, 5857, 1999, 1996, 2492, 1997, 7976, 4454, 1999, 1996, 2925, 1012,
+          102 };
+
+    EXPECT_EQ(vec, correct_ids);
+}
+
+//https://github.com/huggingface/transformers/blob/main/docs/source/en/tokenizer_summary.md#subword-tokenization
+TEST_F(TokenizerTestSuit, EN_Subword_tokenization) {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+    std::string text = "I have installed a fortran compiler";
+
+    std::vector<std::string> correct_subwords = {
+   "i", "have", "installed", "a", "fort", "##ran", "compiler"};
+    auto startTime = Time::now();
+    std::vector<std::string> res = en_tokenizer.word_segment(text);
+    auto execTime = get_duration_ms_till_now(startTime);
+    std::cout << "[INFO] split subword takes " << execTime << "ms\n";
 
     EXPECT_EQ(res, correct_subwords);
 }
